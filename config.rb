20b9a628454f9617801525e6ee50a115a0460452
AndriiKot <a_kocyuba@ukr.net>
2024-07-09 22:03:02 +0200

    testing table

---
+++ config.rb
@@ -0,0 +1,31 @@
+LOGOS = { 'babel' => 'babel-10.svg',
+    'css' => 'css-3.svg',
+    'html' => 'html-1.svg',
+    'chatgpt' => 'chatgpt-1.svg',
+    'd3' => 'd3-2.svg',
+    'docker' => 'docker-4.svg',
+    'eslint' => 'eslint-1.svg',
+    'git' => 'git-icon.svg',
+    'grunt' => 'grunt-1.svg',
+    'gulp' => 'gulp-1.svg',
+    'javascript' => 'javascript-1.svg',
+    'less' => 'less.svg',
+    'npm' => 'npm.svg',
+    'postcss' => 'postcss.svg',
+    'postgresql' => 'postgresql.svg',
+    'prettier' => 'prettier-2.svg',
+    'pug' => 'pug.svg',
+    'react' => 'react-2.svg',
+    'react-query' => 'react-query-seeklogo.svg',
+    'ruby' => 'ruby.svg',
+    'vagrant' => 'vagrant.svg',
+    'virtualbox' => 'virtualbox.svg',
+    'visual-studio-code' => 'visual-studio-code-1.svg',
+    'vite' => 'vitejs.svg',
+    'vue' => 'vue-9.svg',
+    'webpack' => 'webpack-icon.svg' 
+}
+
+
+
+
---
+++ greate_table.rb
@@ -0,0 +1,29 @@
+﻿require './config.rb'
+path = 'https://github.com/AndriiKot/___Icons__and__Links___/blob/main/icons/'
+
+
+def create_table(file = '', options, kwargs)
+    options.each do |key|
+        File.open(file, 'a+') do |f|
+            f.puts("<table>")
+            f.puts("<thead>")
+            f.puts("<tr>")
+            f.puts("<th>")
+            f.puts("#{key}")
+            f.puts("<th>")
+            f.puts("</tr>")
+            f.puts("</thead>")
+            f.puts("<tbody>")
+            f.puts("<tr>")
+            f.puts("<td>")
+            f.puts("#{kwargs[key]}")
+            f.puts("<td>")
+            f.puts("</tr>")
+            f.puts("</tbody>")
+            f.puts("</table>")
+        end    
+    end
+end
+
+create_table('./README.md', ['css','html'], LOGOS)
+  
