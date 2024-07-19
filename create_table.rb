6401e972de5331d07b31e6df513864070ce74e50
AndriiKot <a_kocyuba@ukr.net>
2024-07-19 23:40:47 +0200

    update README.md testing 14

--- create_table.rb
+++ create_table.rb
@@ -4,26 +4,29 @@ path = 'https://github.com/AndriiKot/___Icons__and__Links___/blob/main/icons/'
 
 def create_table(file = '', path, options, kwargs)
     File.open(file, 'w') {|file| file.truncate(0) }
-  f.puts("<table>")
 
-    kwargs.each do |key, value|
-        File.open(file, 'a+') do |f|
-            f.puts("<thead>")
-            f.puts("<tr>")
-            f.puts("<th>")
-            f.puts("#{key}")
-            f.puts("</th>")
-            f.puts("</tr>")
-            f.puts("</thead>")
-            f.puts("<tbody>")
-            f.puts("<tr>")
-            f.puts("<td>")
-            f.puts("<img src=#{path}#{value} alt=#{key} width=30 >")
-            f.puts("</td>")
-            f.puts("</tr>")
-            f.puts("</tbody>")
-        end    
-    f.puts("</table>")
+    File.open(file, 'w+') do |f|
+        f.puts("<table>")
+
+        kwargs.each do |key, value|
+                options.size.times do |i|
+                    f.puts("<thead>")
+                    f.puts("<tr>")
+                    f.puts("<th>")
+                    f.puts("#{key}")
+                    f.puts("</th>")
+                    f.puts("</tr>")
+                    f.puts("</thead>")
+                    f.puts("<tbody>")
+                    f.puts("<tr>")
+                    f.puts("<td>")
+                    f.puts("<img src=#{path}#{value} alt=#{key} width=30>")
+                    f.puts("</td>")
+                    f.puts("</tr>")
+                    f.puts("</tbody>")
+                end
+        f.puts("</table>")
+        end
     end
 end
 
