6fc86d18bfa5d87bec9d02326bc5c70b3d3c0d22
AndriiKot <a_kocyuba@ukr.net>
2024-07-19 23:28:04 +0200

    update README.md testing 11

--- create_table.rb
+++ create_table.rb
@@ -7,7 +7,7 @@ def create_table(file = '', path, options, kwargs)
   p options.size
     kwargs.each do |key, value|
         File.open(file, 'a+') do |f|
-            f.puts("<table width=30 height=30>")
+            f.puts("<table width=50>")
             f.puts("<thead>")
             f.puts("<tr>")
             f.puts("<th>")
@@ -18,7 +18,7 @@ def create_table(file = '', path, options, kwargs)
             f.puts("<tbody>")
             f.puts("<tr>")
             f.puts("<td>")
-            f.puts("<img src=#{path}#{value} alt=#{key} width=30 height=30>")
+            f.puts("<img src=#{path}#{value} alt=#{key} width=30 >")
             f.puts("</td>")
             f.puts("</tr>")
             f.puts("</tbody>")
