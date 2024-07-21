3b66b2e10a213cbc6046dc2ffd8ef3c03d31df6b
AndriiKot <a_kocyuba@ukr.net>
2024-07-21 10:29:00 +0200

    update README.md testing 54

--- create_table.rb
+++ create_table.rb
@@ -8,51 +8,50 @@ def create_table(file = '', path, options, kwargs)
     File.open(file, 'w+') do |f|
         f.puts("<table>")
 
-        options.each do |value|
+        kwargs.each do |key, value|
                     f.puts("<thead>")
                     f.puts("<tr>")
                     f.puts("<th height=20>")
-                    f.puts("#{value}")
+                    f.puts("#{key}")
                     f.puts("</th>")
                     f.puts("</tr>")
                     f.puts("</thead>")
                     f.puts("<tbody>")
                     f.puts("<tr>")
                     f.puts("<td height=50 width=50>")
-                    f.puts("<img src=#{path}#{kwargs[value]} alt=#{value}>")
+                    f.puts("<img src=#{path}#{value} alt=#{key}>")
                     f.puts("</td>")
                     f.puts("</tr>")
                     f.puts("</tbody>")
         end
         f.puts("</table>")
     end
-    p "Finished!!!"
 end
 
-create_table('./README.md',path, %w[CSS, PostCSS, SASS, Stylus, Less], LOGOS)
+create_table('./README.md',path, ['CSS'], LOGOS)
   
-# 'CSS' => 'css.svg',
-# 'HTML5' => 'html.svg',
-# 'ChatGPT' => 'chatgpt.svg',
-# 'D3' => 'd3.svg',
-# 'Docker' => 'docker.svg',
-# 'Eslint' => 'eslint.svg',
-# 'Git' => 'git.svg',
-# 'Grunt' => 'grunt.svg',
-# 'Gulp' => 'gulp.svg',
-# 'JavaScript' => 'javascript-1.svg',
-# 'Less' => 'less.svg',
-# 'NPM' => 'npm.svg',
-# 'PostCSS' => 'postcss.svg',
-# 'PostgreSQL' => 'postgresql.svg',
-# 'Prettier' => 'prettier.svg',
-# 'Pug' => 'pug.svg',
-# 'React' => 'react.svg',
-# 'React-Query' => 'react-query.svg',
-# 'Ruby' => 'ruby.svg',
-# 'Vagrant' => 'vagrant.svg',
-# 'VirtualBox' => 'virtualbox.svg',
-# 'Visual Studio Code' => 'visual-studio-code.svg',
-# 'Vite' => 'vitejs.svg',
-# 'Vue' => 'vue.svg',
-# 'Webpack' => 'webpack.svg' 
+    # 'CSS' => 'css.svg',
+    # 'HTML5' => 'html.svg',
+    # 'ChatGPT' => 'chatgpt.svg',
+    # 'D3' => 'd3.svg',
+    # 'Docker' => 'docker.svg',
+    # 'Eslint' => 'eslint.svg',
+    # 'Git' => 'git.svg',
+    # 'Grunt' => 'grunt.svg',
+    # 'Gulp' => 'gulp.svg',
+    # 'JavaScript' => 'javascript-1.svg',
+    # 'Less' => 'less.svg',
+    # 'NPM' => 'npm.svg',
+    # 'PostCSS' => 'postcss.svg',
+    # 'PostgreSQL' => 'postgresql.svg',
+    # 'Prettier' => 'prettier.svg',
+    # 'Pug' => 'pug.svg',
+    # 'React' => 'react.svg',
+    # 'React-Query' => 'react-query.svg',
+    # 'Ruby' => 'ruby.svg',
+    # 'Vagrant' => 'vagrant.svg',
+    # 'VirtualBox' => 'virtualbox.svg',
+    # 'Visual Studio Code' => 'visual-studio-code.svg',
+    # 'Vite' => 'vitejs.svg',
+    # 'Vue' => 'vue.svg',
+    # 'Webpack' => 'webpack.svg' 
