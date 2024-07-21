0cab9abc36b59cceeb1d29677541c0addaa14c6f
AndriiKot <a_kocyuba@ukr.net>
2024-07-21 10:14:32 +0200

    update README.md testing 51

--- create_table.rb
+++ create_table.rb
@@ -28,5 +28,30 @@ def create_table(file = '', path, options, kwargs)
     end
 end
 
-create_table('./README.md',path, LOGOS.keys, LOGOS)
+create_table('./README.md',path, %w[CSS, PostCSS, SASS, Stylus, Less], LOGOS)
   
+# 'CSS' => 'css.svg',
+# 'HTML5' => 'html.svg',
+# 'ChatGPT' => 'chatgpt.svg',
+# 'D3' => 'd3.svg',
+# 'Docker' => 'docker.svg',
+# 'Eslint' => 'eslint.svg',
+# 'Git' => 'git.svg',
+# 'Grunt' => 'grunt.svg',
+# 'Gulp' => 'gulp.svg',
+# 'JavaScript' => 'javascript-1.svg',
+# 'Less' => 'less.svg',
+# 'NPM' => 'npm.svg',
+# 'PostCSS' => 'postcss.svg',
+# 'PostgreSQL' => 'postgresql.svg',
+# 'Prettier' => 'prettier.svg',
+# 'Pug' => 'pug.svg',
+# 'React' => 'react.svg',
+# 'React-Query' => 'react-query.svg',
+# 'Ruby' => 'ruby.svg',
+# 'Vagrant' => 'vagrant.svg',
+# 'VirtualBox' => 'virtualbox.svg',
+# 'Visual Studio Code' => 'visual-studio-code.svg',
+# 'Vite' => 'vitejs.svg',
+# 'Vue' => 'vue.svg',
+# 'Webpack' => 'webpack.svg' 
