ad5f24e0454ce9719766d00fb5aa110de08332e4
AndriiKot <a_kocyuba@ukr.net>
2024-08-02 23:53:02 +0200

    icons gem ! testing size td testing 3 !

--- test_runner.rb
+++ test_runner.rb
@@ -2,6 +2,6 @@ require 'yaml'
 
 require './lib/table_creator.rb'
 
-table_creator = TableCreator.new(size: 10, file_to_write: './test_data/README.md')
+table_creator = TableCreator.new(size: 1, file_to_write: './test_data/README.md')
 table_creator.create_or_clear_file
 table_creator.add_tables
